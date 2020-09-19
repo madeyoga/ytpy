21a987b79adbd83986aa8419163e5006a43e186f
MadeYoga <vngla21@gmail.com>
2020-09-19 22:59:56 +0700

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
     name="ytpy",
-    version="19.09.2020",
+    version="20.09.2020",
     packages=find_packages(),
 
     # Project uses reStructuredText, so ensure that the docutils get
@@ -20,6 +24,8 @@ setup(
     author="yeogaa",
     author_email="yeogaa02@gmail.com",
     description="Python asynchronous wrapper for youtube data api v3.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords="youtube-api-v3 python youtube-search asynchronous",
     url="https://github.com/madeyoga/ytpy",
 )
