86eaa50604f1d506124f5bd63e289513e1d0fb92
MadeYoga <vngla21@gmail.com>
2021-01-30 13:48:44 +0700

    Removes ytdownloader example & Add example for searching on youtube music

--- examples/search_youtube_music.py
+++ examples/search_youtube_music.py
@@ -1,17 +1,19 @@
 from ytpy import YoutubeClient
 import asyncio
 import aiohttp
+import os
 
 
 async def main(loop):
     session = aiohttp.ClientSession()
 
-    client = YoutubeClient(session)
-    
-    response = await client.search('yoasobi', language='en')
+    client = YoutubeClient(session, yt_music_key=os.environ["YT_MUSIC_KEY"])
+
+    response = await client.search_music(q='black suit')
     print(response)
 
     await session.close()
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main(loop))
