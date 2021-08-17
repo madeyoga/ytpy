4133df7543db08beb9e1ab1358662c24c709cd43
MadeYoga <vngla21@gmail.com>
2021-08-17 16:20:54 +0700

    Update version 2021.8.17 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setup(
     name="ytpy",
-    version="2021.7.27",
+    version="2021.8.17",
     packages=find_packages(),
 
     # Project uses reStructuredText, so ensure that the docutils get
